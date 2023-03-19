d6af23ebe17319c157e9e38499b733ba7d99e204
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-03-19 19:01:14 +0200

    task 10 complete

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  createCollectionButton: document.querySelector('button[data-create]'),
+  destroyCollectionButton: document.querySelector('button[data-destroy]'),
+  inputNumberCollection: document.querySelector('input'),
+  divBoxesMarkup: document.querySelector('#boxes'),
+};
+
+refs.createCollectionButton.addEventListener('click', createBoxes);
+refs.destroyCollectionButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  amount = refs.inputNumberCollection.value;
+  const divBoxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    // divBoxes.push(divBox);
+    const divBox = document.createElement('div');
+    divBox.style.width = 30 + 10 * i + 'px';
+    divBox.style.height = 30 + 10 * i + 'px';
+    divBox.style.background = getRandomHexColor();
+
+    divBoxes[i] = divBox;
+  }
+  refs.divBoxesMarkup.append(...divBoxes);
+  console.dir(refs.divBoxesMarkup);
+}
+
+function destroyBoxes() {
+  refs.inputNumberCollection.value = '';
+  refs.divBoxesMarkup.innerHTML = '';
+}
+// console.dir(refs.divBoxesMarkup);
